5805c721a02ed10f311b28a536a7dc64128c88bb
James-blend <145216038+James-blend@users.noreply.github.com>
2024-03-07 00:11:45 +0200

    Added configuration to copy manifest.json into dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+// Require the CopyWebpackPlugin
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development', // Change to 'production' for production builds
   entry: {
-    background: './src/background/background.js'/*,
-    popup: './src/popup/popup.js',
-    content: './src/content/content.js'*/
+    background: './src/background/background.js',
+    // Uncomment the following lines as needed when you're ready to include these entries
+    // popup: './src/popup/popup.js',
+    // content: './src/content/content.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -25,4 +28,15 @@ module.exports = {
       },
     ],
   },
+  // Add the plugin configuration here
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        // Here you can add more patterns to copy other files or directories as needed
+        // Example: { from: 'src/popup/popup.html', to: 'popup.html' },
+        // Example: { from: 'src/images', to: 'images' }
+      ],
+    }),
+  ],
 };
